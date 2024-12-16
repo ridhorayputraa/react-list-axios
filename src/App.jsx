7ad13e8250c631dce86e30640448a3d219e3f408
Ridho Ray Putra <ridhorayputra033@Gmail.com>
2024-12-16 11:35:09 +0700

    adding-list-component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// Gunakan theme di sini
+import { Box, ThemeProvider } from "@mui/material";
+
+// Gunakan MUI di sini
+import { Button, Typography } from "@mui/material";
+
+// Import ListMovies (Data JSON)
+import ListMovies from "./containers/ListMovies";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    // Gunakan ThemeProvider di sini
+    // Inject Context Theme di sini
+    <div className="App">
+      <header className="App-header">
+        <Box p={2}>
+          <Typography variant="h5">React List dan Axios</Typography>
+        </Box>
+      </header>
+      {/* Panggil ListMovies Component di sini */}
+      <section style={{ paddingLeft: 16, paddingRight: 16 }}>
+        <ListMovies />
+      </section>
+    </div>
+  );
 }
 
-export default App
+export default App;
